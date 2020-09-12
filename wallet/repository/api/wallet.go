1794206f35a7a1cc3d7633ab8e7fdcf83a1dd6f5
Komang Yogananda Mahaputra Wisna <yoganandamahaputra@gmail.com>
2020-09-12 13:19:03 +0800

    revert log wallet

--- wallet/repository/api/wallet.go
+++ wallet/repository/api/wallet.go
@@ -14,26 +14,23 @@ import (
 
 func (r *walletRepository) UpdateMerchantWallet(c context.Context, merchantID uuid.UUID, amount float64) error {
 	payload := map[string]interface{}{"amount": amount}
-	log.Println(payload)
 	reqBody, err := json.Marshal(payload)
 	var URL string
 	URL = fmt.Sprintf("%s/api/v1/transactions/users/%s/credit", r.APIURL, merchantID)
 
 	req, err := http.NewRequest("PUT", URL, bytes.NewBuffer(reqBody))
 	req.Header.Set("Content-Type", "application/json")
-	body, err := ioutil.ReadAll(req.Body)
-	log.Println(string(body))
 	if err != nil {
 		return err
 	}
-	// client := &http.Client{}
-	// res, err := client.Do(req)
-	// log.Println(URL, res.Status, res.StatusCode)
-	// if err != nil {
-	// 	return err
-	// }
-	// defer res.Body.Close()
+	client := &http.Client{}
+	res, err := client.Do(req)
+	log.Println(URL, res.Status, res.StatusCode)
+	if err != nil {
+		return err
+	}
+	defer res.Body.Close()
 
-	// _, err = ioutil.ReadAll(res.Body)
+	_, err = ioutil.ReadAll(res.Body)
 	return err
 }
