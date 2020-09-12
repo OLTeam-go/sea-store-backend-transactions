a1322345f4bb4ffed06c7994e7d5115aa3ab9400
Komang Yogananda Mahaputra Wisna <yoganandamahaputra@gmail.com>
2020-09-12 06:45:21 +0000

    Komangyogananda readme md

--- wallet/repository/api/wallet.go
+++ wallet/repository/api/wallet.go
@@ -12,18 +12,18 @@ import (
 	"github.com/google/uuid"
 )
 
-func (r *walletRepository) UpdateMerchantWallet(c context.Context, merchantID uuid.UUID, amout float64) error {
-	payload := map[string]interface{}{"amount": amout}
+func (r *walletRepository) UpdateMerchantWallet(c context.Context, merchantID uuid.UUID, amount float64) error {
+	payload := map[string]interface{}{"amount": amount}
 	reqBody, err := json.Marshal(payload)
 	var URL string
 	URL = fmt.Sprintf("%s/api/v1/transactions/users/%s/credit", r.APIURL, merchantID)
 
-	client := &http.Client{}
 	req, err := http.NewRequest("PUT", URL, bytes.NewBuffer(reqBody))
 	req.Header.Set("Content-Type", "application/json")
 	if err != nil {
 		return err
 	}
+	client := &http.Client{}
 	res, err := client.Do(req)
 	log.Println(URL, res.Status, res.StatusCode)
 	if err != nil {
